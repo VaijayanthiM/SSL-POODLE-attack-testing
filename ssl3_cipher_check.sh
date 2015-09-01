fd0698cc564a08d6fc37309005f46b25acfb335e
Vaijayanthi <vaijayanthi.muralidharan@gmail.com>
2015-09-01 16:05:44 -0700

    vj third commit Signed-off-by: Vaijayanthi <vaijayanthi.muralidharan@gmail.com>

--- ssl3_cipher_check.sh
+++ ssl3_cipher_check.sh
@@ -5,7 +5,7 @@
 #                        a targeted server.
 #
 # Author  - Lamar Spells (lamar_spells@symantec.com)
-#Test Test again
+#Test Test Test Test again
 #####################################################################################
 
 if [ $# -lt 1 ] ; then
